eadd4b2e1063135f8ee564ed2ee305af26f79ca2
Amadeus Wieczorek <git@amadeusw.com>
2015-07-29 16:59:41 -0700

    Sets up correct solution structure

---
+++ test/Poker.Test.Game/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Poker.Test.Game
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
